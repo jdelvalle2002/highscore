30e3c7a6fdf917c2234a0ef36b47060530248bec
jdelvalle2002 <63021540+jdelvalle2002@users.noreply.github.com>
2021-03-05 15:46:54 -0300

    Add files via upload

--- highscore/highscore POO.py
+++ highscore/highscore POO.py
@@ -1,6 +1,6 @@
 # simulador del juego highscore
 # juego con tabla de 5x5
-
+# funcionando para un jugador
 import random
 
 class Tablero:
@@ -36,7 +36,7 @@ pos_lugares = ["a1","a2","a3","a4","a5","b1","b2","b3","b4","b5",
 "c1", "c2", "c3", "c4", "c5", "d1", "d2", "d3", "d4", "d5", "e1", "e2", "e3", "e4", "e5"]
 def gen_tablero():
     xd = []
-    a = [0,0,0,0,0]
+    a = [" "," "," "," "," "]
     i = 0
     while i < 5:
         xd.append(a[:])
@@ -73,20 +73,127 @@ def escalas_no7():
 def unpar(fila):
     pass
 
-escalas7 = escalas_7()
-#print(escalas7)
-escalasNo7 = escalas_no7()
-#print(escalasNo7)
+
 # acá sacamos los puntajes
 def contar_puntos(tablero):
+    # funciones aux
+    puntos = 0
+    def escalas_7():
+        lista = []
+        for i in range(3,8):
+            esc = range(i,i+5)
+            lista.append(list(esc))
+        return lista
+    def escalas_no7():
+        lista = []
+        for i in [2,8]:
+            esc = range(i,i+5)
+            lista.append(list(esc))
+        return lista
+    def full_f(lista):
+        full = False
+        for x in lista:
+            if lista.count(x) == 3:
+                for y in lista:
+                    if lista.count(y) == 2:
+                        full = True
+        return full
+    def five(lista):
+        for el in lista:
+            if lista.count(el) == 5:
+                return True
+        return False
+    def poker(lista):
+        for el in lista:
+            if lista.count(el) == 4:
+                return True
+        return False
+    def trio(lista):
+        for el in lista:
+            if lista.count(el) == 3:
+                return True
+        return False                        
+    def dospar(lista):
+        full = False
+        for x in lista:
+            if lista.count(x) == 2:
+                for y in lista:
+                    if lista.count(y) == 2 and x != y:
+                        full = True
+        return full
+    def par(lista):
+        for x in lista:
+            if lista.count(x) == 2:
+                return True
+        return False            
+    e7 = escalas_7()
+    en7 = escalas_no7()
+    
     # filas
-    for fila in tablero:
-        if fila in x:
-            pass
-    # columnas
+    quintetos = tablero[:]
+    for i in range(5):
+        columna = []
+        for j in range(5):
+            columna.append(tablero[j][i])
+        quintetos.append(columna)    
+    diag = []
+    line = []
+    line2 = []
+    for i in range(5):
+        p = tablero[i][i]
+        p2 = tablero[-(i+1)][i]
+        line.append(p)
+        line2.append(p2)
+    diag.append(line)
+    diag.append(line2)    
+    for fila in quintetos:
+        f  = sorted(fila)
+        # ojo, acá importa el orden en q se llaman las funciones
+        if f in e7:
+            puntos += 8
+        elif f in en7:
+            puntos += 12
+        elif full_f(f): # acá los full 
+            puntos += 8
+        elif five(f):
+            puntos += 10
+        elif poker(f):
+            puntos += 8
+        elif trio(f):
+            puntos += 3
+        elif dospar(f):
+            puntos += 3
+        elif par(f):
+            puntos += 1                     
     # diagonales
-    pass
+    for fila in diag:
+        f  = sorted(fila)
+        # ojo, acá importa el orden en q se llaman las funciones
+        if f in e7:
+            puntos += 16
+            print(f"La diagonal {f} suma 16 pts")
+        elif f in en7:
+            puntos += 24
+        elif full_f(f): # acá los full 
+            puntos += 16
+        elif five(f):
+            puntos += 20
+        elif poker(f):
+            puntos += 16
+        elif trio(f):
+            puntos += 6
+        elif dospar(f):
+            puntos += 6
+        elif par(f):
+            puntos += 2
+    print("Diagonales")
+    #print(diag)
+    print("Filas y col")
+    print(quintetos)
+    return puntos
 
+
+# para contar partir primero por las que ocupan más espacio
 
 #times = contar(valores)
 #print(times)
@@ -95,6 +202,9 @@ Player1 = Tablero("Joaco")
 Player1.gen_tablero()
 used = [] # lista de posiciones usadas
 disp = pos_lugares[:]
+for line in Player1.matriz:
+    print(line)
+print("Inicia el Juego!")    
 for i in range(1,26):
     print(f"-+-+-+- RONDA {i} -+-+-+-")
     valor = dado()
@@ -107,16 +217,18 @@ for i in range(1,26):
         coor = jug.strip().lower()
         if coor not in pos_lugares:
             print("Posición inválida, intenta de nuevo.")
+        if coor in used:
+            print(f"Ya colocaste un número en la posición {coor}.")
         else:
-            if coor not in used:
-                Player1.colocar(coor, valor)
-                print(f"Pusiste un {valor} en la posición {coor}.")
-                disp.remove(coor)
-                go = False
-            else:
-                print(f"Ya colocaste un número en la posición {coor}.") 
+            Player1.colocar(coor, valor)
+            print(f"Pusiste un {valor} en la posición {coor}.")
+            disp.remove(coor)
+            used.append(coor)
+            go = False
 print("FIN DEL JUEGO\nContemos el puntaje...")
 print("Tablero del jugador")
 
 for linea in Player1.matriz:
     print(linea)
+Player1.p = contar_puntos(Player1.matriz)
+print("El puntaje es", Player1.p)
